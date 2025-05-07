f19e71872f35b53124459a7920a98c12e66e3418
Armando Gonzalez <armndo.g@gmail.com>
2025-05-07 12:12:37 -0600

    feat: testing new functionality

--- public/index.php
+++ public/index.php
@@ -13,7 +13,11 @@ use Controllers\ShiftController;
 use Controllers\StudentController;
 use Controllers\SubjectController;
 use Controllers\UserController;
+use Core\Model;
 use Core\Router;
+use Models\Group;
+use Models\Major;
+use Models\Shift;
 
 Router::get("/majors", [MajorController::class, "index"]);
 Router::post("/majors", [MajorController::class, "store"]);
@@ -47,5 +51,15 @@ Router::get("/students", [StudentController::class, "index"]);
 Router::post("/students", [StudentController::class, "store"]);
 Router::get("/students/{id}", [StudentController::class, "view"]);
 Router::post("/students/{id}", [StudentController::class, "update"]);
+
+Router::get("/", function() {
+  // print("lmao\n");
+
+  // $tmp = Group::select("shifts.*")->join("majors", "majors.id", "groups.major_id")->join("shifts", "shifts.id", "groups.shift_id")->first(Shift::class);
+  $tmp = Group::select("shifts.*")->join("majors", "majors.id", "groups.major_id")->join("shifts", "shifts.id", "groups.shift_id")->find(2);
+  // var_dump($tmp);
+
+  return $tmp;
+});
 
 print(Router::resolve());
